340016e8204a917918e3d4d43a93528b1c5c717f
Iaroslav S <ziro-x-100@mail.ru>
2024-08-24 14:57:33 +0300

    Изменил иконки и добавил наведение

--- frontend/src/assets/svg/MinusIcon.jsx
+++ frontend/src/assets/svg/MinusIcon.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
+import "./Icons.scss";
 
 export default function MinusIcon({ onClick }) {
     return (
         <svg
             onClick={onClick}
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            viewBox="0 0 512 512"
             className="icon minusIcon"
             style={{ cursor: 'pointer' }}
         >
-            <line x1="5" y1="12" x2="19" y2="12" />
+            <path d="M439 7c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8l-144 0c-13.3 0-24-10.7-24-24l0-144c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39L439 7zM72 272l144 0c13.3 0 24 10.7 24 24l0 144c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39L73 505c-9.4 9.4-24.6 9.4-33.9 0L7 473c-9.4-9.4-9.4-24.6 0-33.9l87-87L55 313c-6.9-6.9-8.9-17.2-5.2-26.2s12.5-14.8 22.2-14.8z"/>
         </svg>
     );
 }
